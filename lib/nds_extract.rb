345fd8eebf9f34b813a184f91528eca2040f5e2d
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-18 04:10:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,10 +2,11 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 retuire 'pp'
 
+pretty_data = pp directors_database
+
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  pp directors_database
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
